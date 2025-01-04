adc70496a2df2788aed732f6b8d80d9cd140f6bc
AF <gulabsinghrajput008@gmail.com>
2025-01-04 07:18:47 +0530

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -50,7 +50,7 @@ class Config(object):
     
     LOGGER = logging
 
-    #UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002168191467")
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002168191467")
     
     OWNER_ID = int(os.environ.get("OWNER_ID", "1114789110"))
     
