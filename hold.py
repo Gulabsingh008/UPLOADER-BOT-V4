98e8c92ed695d26e4a4f323bb7e0b0c17894c06e
AF <gulabsinghrajput008@gmail.com>
2025-06-18 22:03:30 +0530

    Update hold.py

--- hold.py
+++ hold.py
@@ -1,13 +1,1 @@
-import requests
-import time
-
-url = "https://ethnic-cristen-vonex-8cc6d411.koyeb.app"
-
-while True:
-    try:
-        response = requests.get(url)
-        print("Status Code:", response.status_code)
-    except requests.exceptions.RequestException as e:
-        print("An error occurred:", e)
-    
-    time.sleep(20)
+#
