2ce937d5a1b4c49f4ba97de37e5c5f3077c3c205
AF <gulabsinghrajput008@gmail.com>
2025-06-18 21:42:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-import os
+#import os
 
 app = Flask(__name__)
 
@@ -7,8 +7,8 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World!'
 
-port = int(os.environ.get("PORT", 8080))
+#port = int(os.environ.get("PORT", 8080))
 
 if __name__ == '__main__':
     # Run the Flask app on port 80
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=80)
