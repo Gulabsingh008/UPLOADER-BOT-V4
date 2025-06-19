d5bf52ee576f6553898496361c7a2f3b627cf96b
AF <gulabsinghrajput008@gmail.com>
2025-06-19 13:08:19 +0530

    Update script.py

--- plugins/script.py
+++ plugins/script.py
@@ -3,7 +3,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 class Translation(object):
 
     START_TEXT = """
-👋 ʜᴇʏ {}!
+👋 ʜᴇʏ {username}
 
 🎉 ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴛʜᴇ ᴜʟᴛɪᴍᴀᴛᴇ ᴅᴏᴡɴʟᴏᴀᴅ ʙᴏᴛ 🤖✨  
 ɪ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ɢʀᴀʙ ᴍᴇᴅɪᴀ ꜰʀᴏᴍ ᴀʟʟ ᴏᴠᴇʀ ᴛʜᴇ ɪɴᴛᴇʀɴᴇᴛ 🌐
