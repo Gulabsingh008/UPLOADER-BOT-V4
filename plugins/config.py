f4b547bf4ba4a4a3340ff1bca75622f02933c2e7
AF <gulabsinghrajput008@gmail.com>
2025-06-20 06:37:12 +0530

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -11,7 +11,7 @@ logging.basicConfig(
 
 class Config(object):
     
-    BOT_TOKEN = os.environ.get("BOT_TOKEN" , "7788977795:AAHTwaDqu_CJiaFFWC7e1vHaMBorPKg6Wo0")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN" , "")
     API_ID = int(os.environ.get("API_ID", 24519654))
     API_HASH = os.environ.get("API_HASH", "1ccea9c29a420df6a6622383fbd83bcd")
     
